09e1f2458fed989f5e18ce6a265e07a11fae75c8
FireChickenProductivity <107892169+FireChickenProductivity@users.noreply.github.com>
2022-12-14 19:41:34 -0700

    Use number of words as position
    Use number of words as position to improve reliability of movement command.

--- draft_window_position.py
+++ draft_window_position.py
@@ -8,15 +8,15 @@ class Actions:
         actions.edit.extend_line_start()
         selected_text = actions.user.fire_chicken_dictation_get_selected_text()
         actions.edit.right()
-        position = len(selected_text)
-        print(selected_text)
+        position = len(selected_text.split(' ')) + 1
         return position
     
     def fire_chicken_draft_window_go_to_position(position: int):
         ''''''
-        actions.user.file_start()
+        actions.edit.extend_line_start()
         if position > 0:
             for i in range(position - 1):
-                actions.edit.right()
+                actions.edit.word_right()
+
 
 
