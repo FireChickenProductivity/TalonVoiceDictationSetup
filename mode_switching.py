d2dc03a41760efed47eba995709b1bab70c5d4ad
FireChickenProductivity <107892169+FireChickenProductivity@users.noreply.github.com>
2023-08-05 18:21:08 -0600

    Remove disabling of debug mode
    Remove disabling of debug mode to improve compatibility with newer community repository versions.

--- mode_switching.py
+++ mode_switching.py
@@ -9,7 +9,6 @@ class Actions:
         actions.mode.disable("command")
         actions.mode.enable("dictation")
         actions.user.code_clear_language_mode()
-        actions.mode.disable("user.gdb")
     def fire_chicken_dictation_enable_command_mode_from_dictation_mode():
         '''Enables command mode from dictation mode'''
         actions.mode.disable("dictation")
